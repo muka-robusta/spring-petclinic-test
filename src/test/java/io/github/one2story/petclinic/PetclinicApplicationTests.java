c3c5d65a0c25c07f67af40dc4186e02f13d3557e
Illia Savenko <illiafun@yandex.ua>
2020-10-07 18:54:57 +0300

    init commit

---
+++ src/test/java/io/github/one2story/petclinic/PetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.one2story.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
