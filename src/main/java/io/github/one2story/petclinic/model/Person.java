17abcec829dc7533afb33a0ff4a373636baf109c
Illia Savenko <illiafun@yandex.ua>
2020-10-10 16:24:54 +0300

    Altering POJOs. Closes #2.

--- src/main/java/io/github/one2story/petclinic/model/Person.java
+++ src/main/java/io/github/one2story/petclinic/model/Person.java
@@ -1,14 +1,10 @@
 package io.github.one2story.petclinic.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Person {
     private String firstName;
     private String lastName;
