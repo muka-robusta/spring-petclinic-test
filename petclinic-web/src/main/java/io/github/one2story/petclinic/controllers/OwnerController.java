59689a78a3f6c6d47855ff68406247701e1cd0bf
Illia Savenko <illiafun@yandex.ua>
2021-01-18 13:07:34 +0200

    Add owners index page and controller. Closes #13

---
+++ petclinic-web/src/main/java/io/github/one2story/petclinic/controllers/OwnerController.java
@@ -0,0 +1,16 @@
+package io.github.one2story.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String ownersList() {
+
+        return "owners/index";
+    }
+
+}
