8d3f2908b91b9910615984bc9fb83e3cddf2a64e
Illia Savenko <illiafun@yandex.ua>
2021-01-19 14:48:48 +0200

    List all owners on owners index page. Closes #16

--- petclinic-web/src/main/java/io/github/one2story/petclinic/controllers/OwnerController.java
+++ petclinic-web/src/main/java/io/github/one2story/petclinic/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package io.github.one2story.petclinic.controllers;
 
+import io.github.one2story.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String ownersList() {
+    public String ownersList(Model model) {
+
+        model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
     }
