f6546295728e861184e727918c4a3e3571d347ed
Illia Savenko <illiafun@yandex.ua>
2021-01-20 18:37:02 +0200

    Add contact properties information to Owner entity. Closes #28

--- petclinic-data/src/main/java/io/github/one2story/petclinic/model/Owner.java
+++ petclinic-data/src/main/java/io/github/one2story/petclinic/model/Owner.java
@@ -1,15 +1,16 @@
 package io.github.one2story.petclinic.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Set;
 
+@Getter
+@Setter
 public class Owner extends Person {
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
 
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
-    }
 }
