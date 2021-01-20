a2e7baa3f7779ab7df6c9eaaa99279292c094909
Illia Savenko <illiafun@yandex.ua>
2021-01-20 18:30:42 +0200

    Create speciality entity and linked id with vet entity. Closes #27

--- petclinic-data/src/main/java/io/github/one2story/petclinic/model/Speciality.java
+++ petclinic-data/src/main/java/io/github/one2story/petclinic/model/Speciality.java
@@ -2,10 +2,9 @@ package io.github.one2story.petclinic.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import java.util.Set;
 
 @Getter
 @Setter
-public class Vet extends Person {
-    private Set<Speciality> specialities;
+public class Speciality extends BaseEntity{
+    private String description;
 }
