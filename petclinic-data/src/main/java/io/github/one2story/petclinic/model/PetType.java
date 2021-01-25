d2dca256b242e07dd8b54de55f0d90cf70722d48
Illia Savenko <illiafun@yandex.ua>
2021-01-25 10:38:22 +0200

    Convert owners to JPA entities. Closes #36

--- petclinic-data/src/main/java/io/github/one2story/petclinic/model/PetType.java
+++ petclinic-data/src/main/java/io/github/one2story/petclinic/model/PetType.java
@@ -4,9 +4,16 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
 
+    @Column(name = "name")
     private String name;
 }
