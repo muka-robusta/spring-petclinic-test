1c142e4ff2dd8645bdb594e8f4752bee0e98d960
Illia Savenko <illiafun@yandex.ua>
2021-01-26 14:52:13 +0200

    Create Visit map service. Closes #46

---
+++ petclinic-data/src/main/java/io/github/one2story/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package io.github.one2story.petclinic.services;
+
+import io.github.one2story.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ petclinic-data/src/main/java/io/github/one2story/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,43 @@
+package io.github.one2story.petclinic.services.map;
+
+import io.github.one2story.petclinic.model.Visit;
+import io.github.one2story.petclinic.services.PetService;
+import io.github.one2story.petclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.findById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if (visit.getPet() == null || visit.getPet().getOwner() == null
+                || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null)
+            throw new RuntimeException("Invalid Visit");
+
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return null;
+    }
+}
